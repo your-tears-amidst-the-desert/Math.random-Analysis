589209e10735eb4d748771a076a888fbe8902386
your-tears-amidst-the-desert <110824455+your-tears-amidst-the-desert@users.noreply.github.com>
2022-08-18 02:16:35 +0400

    Анализ Math.random()

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
-//Как итог работы программы получаем весьма различные результаты для оценки качества работы Math.random() - 
-//значения автокорреляционной функции может быть как < |0.1|, так и значительно превосходить его. Зависит от конкретной итерации, внутреннего состояния системы
-//условие для проверки качества ДПСЧ было взято с сайта - https://intuit.ru/studies/courses/623/479/lecture/21088?page=3 
+//Как итог работы программы получаем весьма хорошие результаты для оценки качества работы Math.random().
+//Значения автокорреляционной функции всегда < |0.1|, лишь изредка приближаясь к пороговому значению, а мат. ожидание колеблется в пределах +-0.017 от ожидаемого.
+//На основании данного наблюдения, реализацию метода Math.random() можно счесть положительной.
+//Условие для проверки качества ДПСЧ было взято с сайта - https://intuit.ru/studies/courses/623/479/lecture/21088?page=3 
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -14,17 +15,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class TEST {
-    
     static String funcName = "График функции";
     static String frameName = "Тестовое задание";
     static String graphicName = "y = F(x)";
     static XYSeries series1 = new XYSeries(funcName);
     static XYSeries series2 = new XYSeries(funcName);
-    static String densityName = "y = F'(x) = f(x)";
+    static String densityName = "Отсортированная последовательность СВ";
 
-    //округление до нужного знака
     public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
 
@@ -34,26 +34,28 @@ public class TEST {
         return (double) tmp / factor;
     }
 
+    public static ArrayList<Double> zadanie(double akf) {
+        return zadanie(1);
+    }
+
     public static void main(String[] args) {
-        
+        double start, stop, totalTime;
+        start = System.currentTimeMillis();
         final int scale = 1000;
         final int shift = 7;
         final int range = scale - shift;
         double Dx = 0.0;
         double Mx = 0.0;
-        int sumElements = 0;
 
-        //генерация последовательности
         ArrayList<Double> counts = new ArrayList<>();
         double x;
         double y;
         for(int i = 0; i < scale; i++) {
             x = Math.random();
-            //y = Math.pow(1-x, Math.sin(Math.E - x)) * i / 1;
+            y = Math.pow(1-x, Math.sin(Math.E - x)) * i / 1;
             counts.add(x);
         }
 
-        //вычисляем центрированную корреляционную функцию
         double sum = 0;
         double R = ((double) 1 / range);
         ArrayList<Double> r = new ArrayList<>();
@@ -68,38 +70,38 @@ public class TEST {
         ArrayList<Double> countsSorted = new ArrayList<>(counts);
         Collections.sort(countsSorted);
 
-        //вычисляем математическое ожидание и дисперсию случайной величины
         ArrayList<Integer> density = new ArrayList<>();
-        int count = 0;
-        double chance = 0.0;
-        for(double i = 0; i < (double)countsSorted.size() / scale; i += 0.001) {
-            if(i == round(countsSorted.get((int)i * scale), 3)) {
-                count++;
+        List<Double> chance = new ArrayList<>();
+        double mx = 0;
+        double dx = 0;
+        for(double i = 0; i < (double)(countsSorted.size() / scale); i += 0.001) {
+            int count = 0;
+            for(int j = 0; j < scale - 1; j++) {
+                if (round(countsSorted.get((int)(i * scale)), 6) == round(countsSorted.get(j+1), 6)) {
+                    count++;
+                }
             }
-            chance = (double)count / scale;
+            chance.add( (double)count / scale );
             try {
-                double mx;
-                double dx = 0;
-                mx = i * chance;
+                mx = counts.get((int)(i * scale)) * chance.get((int)(i * scale));
                 if (mx != 0) {
-                    dx = Math.pow(countsSorted.get((int)(i * scale)) - 0.5, 2) * chance;
+                    dx = Math.pow(countsSorted.get((int)(i * scale)) - 0.5, 2) * chance.get((int)(i * scale));
                 }
                 Mx += mx;
                 Dx += dx;
             } catch (ArithmeticException e) {
                 System.out.println("Ошибка!");
             }
-            sumElements += count;
             density.add(count);
         }
+        //System.out.println(chance);
 
-        //принтуем итоги 
-        System.out.println("Всего элементов = " + sumElements);
+        System.out.println("Всего элементов = " + scale);
         System.out.println("Математическое ожидание = " + Mx);
         System.out.println("Дисперсия = " + Dx);
         System.out.println("Центрированная корреляционная функция = " + R + ";");
 
-        double normalizedR = R / Dx / Dx;
+        double normalizedR = R / Dx;
         System.out.println("Нормированная корреляционная функция(автокорреляционная ф.) = " + normalizedR + ";");
 
         if (-0.1 < normalizedR && normalizedR < 0.1) {
@@ -108,16 +110,14 @@ public class TEST {
             System.out.println('\n' + "Итог: ПЛОХОЙ ДПСЧ( > |0.1| );" + '\n');
         }
 
-        //добавляем данные в датасет для визуализации на графике
-        System.out.println(density);
-        System.out.println(countsSorted);
+        //System.out.println(density);
+        //System.out.println(countsSorted);
         for (double i = 0; i < 1; i += 0.001) {
             series1.add(i, counts.get((int)(i*scale)));
             //series2.add(i, density.get((int)(i * scale)));
             series2.add(i, countsSorted.get((int)(i * scale)));
         }
 
-        //создание фрэйма и помещение в него графика
         XYDataset xyDataset1 = new XYSeriesCollection(series1);
         XYDataset xyDataset2 = new XYSeriesCollection(series2);
         JFreeChart chart1 = ChartFactory.createXYLineChart(graphicName, "x", "y", xyDataset1, PlotOrientation.VERTICAL, true, true, true);
@@ -131,5 +131,9 @@ public class TEST {
         frame.setLocation(360, 240);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        stop = System.currentTimeMillis();
+        totalTime = (stop-start) / 1000.0;
+        System.out.println("Всего прошло " + totalTime + " секунды.");
     }
 }
